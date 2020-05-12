1c63096151638d06102d714361ddbb3584a16c7c
spider-binbai <56785039+spider-binbai@users.noreply.github.com>
2020-05-12 11:14:01 +0800

    landing animations

--- js/app.js
+++ js/app.js
@@ -19,5 +19,7 @@ var tl = gsap.timeline({defaults:{duration: 1}});
 tl
     .from('.name' , {x:-50, opacity: 0, duration: 1.5,  ease : 'Slow.easeInOut'})
     .from('.lastname' , {x:50, opacity: 0,  ease : 'Slow.easeInOut'}, "-=.5")
-    .from('.description', {opacity: 0,  ease : 'Slow.easeInOut'}, "-=.5")
     .from('.avatar', {y:20, opacity: 0, duration: 2, ease : 'Back.easeInOut'},"-=1")
+    .from('.description', {opacity: 0,  ease : 'Slow.easeInOut'}, "-=.5")
+    .from('.stagger', {opacity: 0, x:-50,  ease : 'Slow.easeInOut', stagger:1}, "-=.5")
+    .from('.explore' , {y:20, opacity: 0 , ease : 'Slow.easeInOut'})
